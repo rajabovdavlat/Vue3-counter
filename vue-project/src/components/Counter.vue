b6238add2ae15614c7550b39024498a271f44885
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-23 16:37:01 +0500

    I added CSS styles and changed the color to make it look nicer.

--- vue-project/src/components/Counter.vue
+++ vue-project/src/components/Counter.vue
@@ -1,8 +1,9 @@
 <template>
-  <h1>{{ count }}</h1>
-  <button @click="inc">+1</button>
+  <div class="counter-wrapp">
+    <h1 class="counter">{{ count }}</h1>
+    <button class="counter-btn" @click="inc">+1</button>
+  </div>
 </template>
-
 
 <!-- methods Options API -->
 <script>
@@ -17,3 +18,32 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.counter-wrapp {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+.counter {
+   font-weight: 700;
+   font-size: 50px;
+   color: brown;
+}
+.counter-btn{
+    font-weight: 900;
+    padding: 10px 24px 10px;
+    font-size: 12px;
+    border-radius: 10px;
+    border: none;
+    background-color: blue;
+    color: azure;
+}
+.counter-btn:hover{
+    border: 1px  solid #fff;
+    background-color: #fff;
+    color: black;
+}
+</style>
