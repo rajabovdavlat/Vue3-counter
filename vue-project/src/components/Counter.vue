cfecb5e6075238b1409bbd7f9311c38638a2236a
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-23 16:51:59 +0500

    added a clear values ​​button

--- vue-project/src/components/Counter.vue
+++ vue-project/src/components/Counter.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="counter-wrapp">
     <h1 class="counter">{{ count }}</h1>
-    <button class="counter-btn" @click="inc">+1</button>
+    <div class="buttons">
+      <button class="counter-btn1" @click="inc">+1</button>
+      <button class="counter-btn2" @click="reset">reset</button>
+    </div>
   </div>
 </template>
 
@@ -15,35 +18,60 @@ export default {
     inc() {
       this.count++;
     },
+    reset() {
+      this.count = 0;
+    },
   },
 };
 </script>
 
 <style scoped>
 .counter-wrapp {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
 .counter {
-   font-weight: 700;
-   font-size: 50px;
-   color: brown;
+  font-weight: 700;
+  font-size: 50px;
+  color: brown;
 }
-.counter-btn{
-    font-weight: 900;
-    padding: 10px 24px 10px;
-    font-size: 12px;
-    border-radius: 10px;
-    border: none;
-    background-color: blue;
-    color: azure;
+.buttons {
+  display: flex;
+  gap: 20px;
 }
-.counter-btn:hover{
-    border: 1px  solid #fff;
-    background-color: #fff;
-    color: black;
+
+/* button-first */
+.counter-btn1 {
+  font-weight: 900;
+  padding: 10px 24px 10px;
+  font-size: 12px;
+  border-radius: 10px;
+  border: blue;
+  background-color: blue;
+  color: azure;
+}
+.counter-btn1:hover {
+  border: 1px solid #fff;
+  background-color: #fff;
+  color: black;
+}
+
+/* button-twщ */
+.counter-btn2 {
+  font-weight: 900;
+  padding: 10px 24px 10px;
+  font-size: 12px;
+  border-radius: 10px;
+  border: red;
+  background-color: red;
+  color: azure;
+}
+.counter-btn2:hover {
+  border: 1px solid #fff;
+  background-color: #fff;
+  color: black;
 }
 </style>
