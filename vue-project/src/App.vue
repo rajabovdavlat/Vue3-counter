6b6b38ba5aff8025d3aba9d4a78902b4ab32c2c8
rajabovdavlat <rajabovoff1@gmail.com>
2025-12-23 16:16:15 +0500

    A simple counter in Vue 3

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Counter from './components/Counter.vue'
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
+
+<template>
+  <Counter />
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ vue-project/src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="inc">+1</button>
+</template>
+
+
+<!-- methods Options API -->
+<script>
+export default {
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    inc() {
+      this.count++;
+    },
+  },
+};
+</script>
